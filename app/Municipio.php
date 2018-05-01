68deef0263f3f98a98ea51cca79534d8bb7f5c4f
L-Ghost <luis.arantes.sp@gmail.com>
2018-04-30 22:16:36 -0300

    Eloquent Model - regioes e municipios

---
+++ app/Municipio.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Municipio extends Model
+{
+    protected $table = 'municipios';
+
+}
---
+++ app/Regiao.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Regiao extends Model
+{
+    protected $table = 'regioes';
+
+}
