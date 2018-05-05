4fdd44c3f5ecc5da639e84bcba6be1ff390286fd
Douglas V Pasqua <douglas.pasqua@forseti.com.br>
2018-05-05 11:55:22 -0300

    models

---
+++ app/Fornecedor.php
@@ -0,0 +1,9 @@
+<?php
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Fornecedor extends Model
+{
+    protected $table = 'fornecedores';
+}
---
+++ app/Proposta.php
@@ -0,0 +1,9 @@
+<?php
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Proposta extends Model
+{
+    protected $table = 'propostas';
+}
